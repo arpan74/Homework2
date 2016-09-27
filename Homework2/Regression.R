9895459c98a0d352b8005d09a61d965fa249395a
Arpan Gupta <arpan74@gmail.com>
2016-09-27 10:34:25 -0400

    Added CI and PI intervals

--- Homework2/Regression.R
+++ Homework2/Regression.R
@@ -10,8 +10,20 @@ abline(lm(BoxOffice~Rating, data = DFNew), col="green")
 summary(lm(BoxOffice~Rating, data = DFNew))
 legend("topleft",c("New Model","Old model"),lty=c(1,1),lwd=c(2.5,2.5),col=c("green","red"))
 
+
+
 model2 <- lm(BoxOffice ~ Rating, data = DFNew) # new model without outliers and shit
-plot(model2)# first plot is fitted vs resids
+plot(model2) 
 model2.stdres = rstandard(model2)
-qqnorm(model2.stdres, ylab="Residuals", xlab="Normal Scores", main="Normal Plot of Residuals") 
+qqnorm(model2.stdres, ylab="Standardized Residuals", xlab="Normal Scores", main="Normal Plot of Residuals") 
 qqline(model2.stdres)
+
+plot(DFNew$Rating, (DFNew$BoxOffice), main = "Rotten Tomatoes Rating vs Domestic Box Office Revenues", xlab = "Rotten Tomates Rating (0 - 100%)", ylab = "US Box Office Revenues (millions of dollars)")
+abline(lm(BoxOffice~Rating, data = DFNew), col="green")
+lines(DFNew$Rating, predict(model2, DFNew, interval="confidence")[,2], lty=2)
+lines(DFNew$Rating, predict(model2, DFNew, interval="confidence")[,3], lty=2)
+
+plot(DFNew$Rating, (DFNew$BoxOffice), main = "Rotten Tomatoes Rating vs Domestic Box Office Revenues", xlab = "Rotten Tomates Rating (0 - 100%)", ylab = "US Box Office Revenues (millions of dollars)")
+abline(lm(BoxOffice~Rating, data = DFNew), col="green")
+lines(DFNew$Rating, predict(model2, DFNew, interval="prediction")[,2], lty=2)
+lines(DFNew$Rating, predict(model2, DFNew, interval="prediction")[,3], lty=2)
