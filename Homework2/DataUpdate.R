f517fc110b89a852ffd84e4c08395a683ad8ed4d
Arpan Gupta <arpan74@gmail.com>
2016-09-27 01:05:48 -0400

    wrote more code

--- Homework2/DataUpdate.R
+++ Homework2/DataUpdate.R
@@ -1,4 +1,6 @@
 DF <- read.csv("Movies.csv", header = FALSE)
 colnames(DF) <- c("Name", "BoxOffice", "Rating", "Year")
-
+DF$BoxOffice <- DF$BoxOffice / 1000000
+remove <- c(17, 20, 32,33)
+DFNew <- DF[setdiff(c(1:40), remove),]
 
--- Homework2/Regression.R
+++ Homework2/Regression.R
@@ -1,4 +1,17 @@
 
-plot(DF$Rating, (DF$BoxOffice / 1000000), main = "Rotten Tomatoes Rating vs Domestic Box Office Revenues", xlab = "Rotten Tomates Rating (0 - 100%)", ylab = "US Box Office Revenues (millions of dollars)")
-model <- lm(BoxOffice ~ Rating, data = DF)
-summary(model)
+plot(DF$Rating, (DF$BoxOffice), main = "Rotten Tomatoes Rating vs Domestic Box Office Revenues", xlab = "Rotten Tomates Rating (0 - 100%)", ylab = "US Box Office Revenues (millions of dollars)")
+model1 <- lm(BoxOffice ~ Rating, data = DF)
+summary(model1)
+plot(DF$Rating, (DF$BoxOffice), main = "Rotten Tomatoes Rating vs Domestic Box Office Revenues", xlab = "Rotten Tomates Rating (0 - 100%)", ylab = "US Box Office Revenues (millions of dollars)")
+abline(lm(BoxOffice~Rating, data = DF), col="red")
+identify(DF$Rating, DF$BoxOffice)
+DFNew <- DF[setdiff(c(1:40), remove),]
+abline(lm(BoxOffice~Rating, data = DFNew), col="green")
+summary(lm(BoxOffice~Rating, data = DFNew))
+legend("topleft",c("New Model","Old model"),lty=c(1,1),lwd=c(2.5,2.5),col=c("green","red"))
+
+model2 <- lm(BoxOffice ~ Rating, data = DFNew) # new model without outliers and shit
+plot(model2)# first plot is fitted vs resids
+model2.stdres = rstandard(model2)
+qqnorm(model2.stdres, ylab="Residuals", xlab="Normal Scores", main="Normal Plot of Residuals") 
+qqline(model2.stdres)
